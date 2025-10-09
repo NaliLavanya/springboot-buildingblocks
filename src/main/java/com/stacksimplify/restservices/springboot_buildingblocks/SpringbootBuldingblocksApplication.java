a900238208800d969cf50ee5883c15e7e959186b
Lavanya nali <lavanyanali0987@gmail.com>
2025-10-09 13:05:41 +0530

    My First Commit

---
+++ src/main/java/com/stacksimplify/restservices/springboot_buildingblocks/SpringbootBuldingblocksApplication.java
@@ -0,0 +1,13 @@
+package com.stacksimplify.restservices.springboot_buildingblocks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBuldingblocksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBuldingblocksApplication.class, args);
+	}
+
+}
