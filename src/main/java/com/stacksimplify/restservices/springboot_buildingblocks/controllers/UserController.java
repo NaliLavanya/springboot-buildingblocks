d66c177414348ea247e579b0153a71b7a0d988fc
Lavanya nali <lavanyanali0987@gmail.com>
2025-10-13 22:33:01 +0530

    ResponseStatusCodeException first commit

--- src/main/java/com/stacksimplify/restservices/springboot_buildingblocks/controllers/UserController.java
+++ src/main/java/com/stacksimplify/restservices/springboot_buildingblocks/controllers/UserController.java
@@ -4,6 +4,9 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,8 +14,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import com.stacksimplify.restservices.springboot_buildingblocks.entities.Users1;
+import com.stacksimplify.restservices.springboot_buildingblocks.exceptions.UserExistsException;
+import com.stacksimplify.restservices.springboot_buildingblocks.exceptions.UserNotFoundException;
 import com.stacksimplify.restservices.springboot_buildingblocks.services.UserServices;
 
 @RestController
@@ -28,19 +35,33 @@ public class UserController {
 	}
 	
 	@PostMapping("/createuser")
-	public Users1 createUser(@RequestBody Users1 users1)
+	public ResponseEntity<Void> createUser(@RequestBody Users1 users1,UriComponentsBuilder builder)
 	{
-		return userService.createUser(users1);
+		try {
+		 userService.createUser(users1);
+		 HttpHeaders header = new HttpHeaders();
+		 header.setLocation(builder.path("/createuser/{id}").buildAndExpand(users1.getId()).toUri());
+		 return new ResponseEntity<Void>(header,HttpStatus.CREATED);
+		}
+		catch(UserExistsException ex)
+		{
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST,ex.getMessage());
+		}
 	}
 	
 	@GetMapping("/users/{id}")
 	public Optional<Users1> getUserById(@PathVariable("id")Long id)
 	{
+		try {
 		return userService.getUserById(id);
+		}
+		catch(UserNotFoundException ex)
+		{
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND,ex.getMessage());
+		}
 	}
 	@PutMapping("/updateuser/{id}")
-	public Users1 updateUserById(@PathVariable("id")Long id,@RequestBody Users1 users1)
-	{
+	public Users1 updateUserById(@PathVariable("id")Long id,@RequestBody Users1 users1) throws UserNotFoundException {
 		//Optional<Users1> users1 = getUserById(id);
 //	users1.setId(100);
 //	users1.setFirstName("ABC");
@@ -49,14 +70,28 @@ public class UserController {
 //	users1.setRole("Admin");
 //	users1.setSsn("SSN010");
 //	
-		return userService.updateUser(users1);
+		try {
+			Optional<Users1> users12 = userService.getUserById(id);
+			 //  Users1 user = users1.orElseThrow(() -> new UserNotFoundException("User not found with id " + id));
+
+			return userService.updateUser(users1);
+		}catch(UserNotFoundException ex)
+		{
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST,ex.getMessage()+"to update");
+		}
 		
 	}
 	
 	@DeleteMapping("/deleteuserbyid/{id}")
 	public void deleteUserById(@PathVariable("id") Long id)
 	{
+		try {
 		userService.deleteUserById(id);
+		}
+		catch(UserNotFoundException ex)
+		{
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND,ex.getMessage());
+		}
 	}
 	
 	@GetMapping("/getuserbyname/{username}")
--- src/main/java/com/stacksimplify/restservices/springboot_buildingblocks/services/UserServices.java
+++ src/main/java/com/stacksimplify/restservices/springboot_buildingblocks/services/UserServices.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.stacksimplify.restservices.springboot_buildingblocks.entities.Users1;
+import com.stacksimplify.restservices.springboot_buildingblocks.exceptions.UserExistsException;
+import com.stacksimplify.restservices.springboot_buildingblocks.exceptions.UserNotFoundException;
 import com.stacksimplify.restservices.springboot_buildingblocks.repositories.Users1Repository;
 
 @Service
@@ -19,13 +21,23 @@ public class UserServices {
 		return users1Repository.findAll();
 	}
 	
-	public Users1 createUser(Users1 users1)
+	public Users1 createUser(Users1 users1) throws UserExistsException
 	{
+		Users1 user = users1Repository.findByUserName(users1.getUserName());
+		if(user!=null)
+		{
+			throw new UserExistsException("user already Exists");
+		}
+		
 		return users1Repository.save(users1);
 	}
 
-	public Optional<Users1> getUserById(Long id) {
+	public Optional<Users1> getUserById(Long id) throws UserNotFoundException{
 		// TODO Auto-generated method stub
+		if(!users1Repository.findById(id).isPresent())
+		{
+			throw new UserNotFoundException("User Not Found Exception in repository");
+		}
 		return users1Repository.findById(id);
 	}
 
@@ -33,11 +45,15 @@ public class UserServices {
 		// TODO Auto-generated method stub
 		return users1Repository.save(users1);
 	}
-	public void deleteUserById(Long id)
+	public void deleteUserById(Long id) throws UserNotFoundException
 	{
 		if(users1Repository.findById(id).isPresent())
 		{
 			users1Repository.deleteById(id);
+		}
+		else
+		{
+			throw new UserNotFoundException("user with id"+id+"not found");
 		}
 	}
 	public Users1 findByUserName(String userName)
